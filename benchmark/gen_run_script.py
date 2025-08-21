af5a6069d83ca603af5dad4a72def82312628fa7
strlst <satorialist@disroot.org>
2025-08-21 15:14:36 +0900

    memsec: benchmark script generates stdin/out/err

--- benchmark/gen_run_script.py
+++ benchmark/gen_run_script.py
@@ -22,9 +22,17 @@ def main(args):
             commands["specdir"], commands["benchmarks"][bench]["command"]
         )
         runs = commands["benchmarks"][bench]["runs"]
-        for run_id, run in enumerate(runs):
+        stdins = commands["benchmarks"][bench]["stdins"]
+        stdouts = commands["benchmarks"][bench]["stdouts"]
+        stderrs = commands["benchmarks"][bench]["stderrs"]
+        for run_id, (run, stdin, stdout, stderr) in enumerate(
+            zip(runs, stdins, stdouts, stderrs)
+        ):
             if args.dry:
-                print(f"{cmd} {run}")
+                cmd_stdin = f" < {stdin}" if stdin else ""
+                cmd_stdout = f" > {stdout}" if stdout else ""
+                cmd_stderr = f" 2> {stderr}" if stderr else ""
+                print(f"{cmd} {run}{cmd_stdin}{cmd_stdout}{cmd_stderr}")
                 continue
             misc = [
                 f.split("/")[-1]
@@ -51,7 +59,18 @@ def main(args):
                         print(f"copying from {joined} to {bench}")
                         shutil.copy(joined, os.path.join("benchmark", bench))
                         break
-            cmds.append((cmd, run, bench, run_id))
+            cmd_stdin = f' SPECSTDIN="--input {stdin}"' if stdin else ""
+            cmd_stdout = f' SPECSTDOUT="--output {stdout}"' if stdout else ""
+            cmd_stderr = f' SPECSTDERR="--errout {stderr}"' if stderr else ""
+            cmds.append(
+                (
+                    cmd,
+                    run,
+                    bench,
+                    run_id,
+                    f"{cmd_stdin}{cmd_stdout}{cmd_stderr}",
+                )
+            )
 
     if args.dry:
         return
@@ -59,12 +78,12 @@ def main(args):
     # for now just print make commands instead of actually calling make
     with open(args.out_path, "w") as out_file:
         out_file.write("#!/bin/sh -x\n")
-        for cmd, options, bench, run_id in cmds:
+        for cmd, options, bench, run_id, extras in cmds:
             suffix = datetime.datetime.now().strftime("%m%d_%H%M")
             outdir = os.path.join("result", f"{bench}_{run_id}_{suffix}")
             os.makedirs(outdir, exist_ok=True)
             print(f"created folder {outdir}")
-            final_cmd = f'time make spec SPECOUTDIR={outdir} SPECCMD={cmd} SPECOPTIONS="{options}" 2>&1 > {outdir}/log &'
+            final_cmd = f'time make spec SPECOUTDIR={outdir} SPECCMD={cmd} SPECOPTIONS="{options}" {extras} 2>&1 > {outdir}/log &'
             out_file.write(f"{final_cmd}\n")
         mode = os.stat(args.out_path).st_mode
         mode |= (mode & 0o444) >> 2
